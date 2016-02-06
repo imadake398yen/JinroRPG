bdc4caab9cdbe735bbf239eb5b58187b19f36961
Minami <MINAMI@nakagawaminami-no-MacBook-Air.local>
2016-02-07 01:39:06 +0900

    おおまかな設計

---
+++ Assets/Scripts/CharacterData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterData : MonoBehaviour {
+
+	public string name;
+	public int  id,
+				role,
+				actRole,
+				belonging,
+				personality,
+				avatar,
+				eatenTurn,
+				fortuneTurn,
+				guardTurn,
+				fakeFortuneTurn,
+				fortuneResult,
+				fakeFortuneResult;
+
+	public void SetCharaData (string csvData) {
+
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+	
+	List<CharacterData> character = new List<CharacterData>();
+	Dictionary<int, CharacterData> characterDictionary = new Dictionary<int, CharacterData>();
+
+	public void SetAllCharacterData (string csvData) {
+
+		string[,] charactersColumn;
+		TextAsset charactersCSV;
+		charactersCSV = Resources.Load("characters") as TextAsset;
+	} 
+}
