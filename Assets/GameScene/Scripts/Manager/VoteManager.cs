459ccb2e9075da1fba2f99e1467d77b488fad60c
falcon <imadake398yen@gmail.com>
2016-11-18 20:17:53 +0900

    VoteManagerを編集

--- Assets/GameScene/Scripts/Manager/VoteManager.cs
+++ Assets/GameScene/Scripts/Manager/VoteManager.cs
@@ -1,7 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class VoteManager : MonoBehaviour {
+public class VoteManager : SingletonMonoBehaviour<VoteManager> {
+
+	public int selector = 0;
+
+	private void Left () {
+		if (selector == 0) {
+			selector = voteControllers.Length - 1;
+		}
+	}
+	private void Right () {
+		if (selector == voteControllers.Length - 1) {
+			selector = 0;
+		}
+	}
+	private void Update () {
+		if (Input.GetKeyDown("right")) Right();
+		if (Input.GetKeyDown("left")) Left();
+	}
+
+
 
 	public VoteController[] voteControllers;
 
