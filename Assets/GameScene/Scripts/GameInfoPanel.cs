88d16e8a836144b2588a384546c6e12e96fb3f60
falcon <imadake398yen@gmail.com>
2017-02-10 20:40:10 +0900

    InfoPanel作った

---
+++ Assets/GameScene/Scripts/GameInfoPanel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameInfoPanel : MonoBehaviour {
+
+	public Text infoLabel;
+
+	private void Update ()
+	{
+		infoLabel.text = "今" + StageManager.instance.day + "日目";
+	}
+
+}
